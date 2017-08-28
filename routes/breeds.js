84ed622b369956485cb923bbe262328e2b63fb1e
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-08-28 11:56:02 -0600

    set up basic get route

---
+++ routes/breeds.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+
+const knex = require('../knex');
+
+const bodyParser = require('body-parser');
+
+router.get('/breeds', (req, res, next) => {
+  knex('breeds')
+    .select([])
+    .then((data) => {
+      res.send(data);
+  })
+  .catch((err) => {
+      console.log(err);
+  });
+
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-const express = require('express'); const app = express();
+'use strict';
+
 const knex = require('./knex');
-const port = process.env.PORT || 9000; app.listen(port, () => { console.log('Listenting on Port', port);
-})
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const breeds = require('./routes/breeds');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use('/',classifieds);
+
+app.use(express.static('public'));
+const port = process.env.PORT || 3000;
+const knex = require('./knex');
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+
+
+module.exports = app;
