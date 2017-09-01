ce5dfb0c80a4ca28637b1c86796411690a326d60
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-09-01 09:51:55 -0600

    update migrations

--- migrations/20170824111200_users.js
+++ migrations/20170824111200_users.js
@@ -4,6 +4,7 @@ exports.up = (knex, Promise) => {
 		table.string('first_name').notNullable();
     table.string('last_name').notNullable();
     table.string('email').notNullable();
+		table.boolean('has_dog').notNullable();
 	});
 };
 
