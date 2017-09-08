48e619a2c6baaaf917b8990a47e45dddfccb5f16
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-09-08 11:53:17 -0600

    udpate server file

--- server.js
+++ server.js
@@ -1,65 +1,22 @@
+'use strict';
+
 const express = require('express');
-const passport = require('passport');
-const FacebookStrategy = require('passport-facebook').Strategy
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-// Import Facebook and Google OAuth apps configs
-// import { facebook, google } from './config';
-
-const { facebook , google } = require('./config');
-
-// Transform Facebook profile because Facebook and Google profile objects look different
-// and we want to transform them into user objects that have the same set of attributes
-const transformFacebookProfile = (profile) => ({
-  name: profile.name,
-  avatar: profile.picture.data.url,
-});
-
-// Transform Google profile into user object
-const transformGoogleProfile = (profile) => ({
-  name: profile.displayName,
-  avatar: profile.image.url,
-});
-
-// Register Facebook Passport strategy
-passport.use(new FacebookStrategy(facebook,
-  async (accessToken, refreshToken, profile, done) => done(null, transformFacebookProfile(profile._json))
-));
-
-// Register Google Passport strategy
-passport.use(new GoogleStrategy(google,
-  async (accessToken, refreshToken, profile, done) => done(null, transformGoogleProfile(profile._json))
-));
-
-// Serialize user into the sessions
-passport.serializeUser((user, done) => done(null, user));
-
-// Deserialize user from the sessions
-passport.deserializeUser((user, done) => done(null, user));
-
-// Initialize http server
 const app = express();
+const bodyParser = require('body-parser');
 
-// Initialize Passport
-app.use(passport.initialize());
-app.use(passport.session());
+const breeds = require('./routes/breeds');
 
-// Set up Facebook auth routes
-app.get('/auth/facebook', passport.authenticate('facebook'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/auth/facebook' }),
-  // Redirect user back to the mobile app using Linking with a custom protocol OAuthLogin
-  (req, res) => res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user)));
+app.use(bodyParser.json());
 
-// Set up Google auth routes
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+app.use('/',breeds);
+app.use(express.static('public'));
 
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/auth/google' }),
-  (req, res) => res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user)));
 
-// Launch the server on the port 3000
-const server = app.listen(3000, () => {
-  const { address, port } = server.address();
-  console.log(`Listening at http://${address}:${port}`);
+const port = process.env.PORT || 3000;
+const knex = require('./knex');
+
+app.listen(port, () => {
+  console.log('Listening on port', port);
 });
